eb3506c1fa0b9c6082f31e540753fb45b17bfc83
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-02-24 22:40:10 -0500

    FPS fixed, now working on rotation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
 from constants import *
-from circleshape import *
-from player import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
@@ -18,8 +18,8 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         player.update(dt)
-        dt = clock.tick(60) / 1000
         pygame.display.update()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
