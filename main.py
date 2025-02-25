874929c49ca3bc151b73454d98b7db5975080147
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-02-24 23:32:22 -0500

    movement complete. creating groups for my objects

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from constants import *
-from circleshape import CircleShape
 from player import Player
 
 def main():
@@ -10,15 +9,21 @@ def main():
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+
+        updatable.update(dt)
+        screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
-        player.update(dt)
-        pygame.display.update()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
