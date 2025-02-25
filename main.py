c967163fbb7198c53b78e168a297dbdb2a83606a
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-02-24 21:23:43 -0500

    need to update FPS before moving forward

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock
+    clock = pygame.time.Clock
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
@@ -17,8 +17,9 @@ def main():
         pygame.Surface.fill(screen, (0,0,0))
         player.draw(screen)
         pygame.display.flip()
-    pygame.time.Clock.tick(60)
-    dt = pygame.time.Clock.tick() / 1000
+        player.update(dt)
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -19,12 +19,13 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+        a = self.rotation + PLAYER_TURN_SPEED * dt
+        return a
 
-        def update(self, dt):
-                keys = pygame.key.get_pressed()
+    def update(self, dt):
+            keys = pygame.key.get_pressed()
 
-                if keys[pygame.K_a]:
-                    # ?
-                if keys[pygame.K_d]:
-                    # ?
+            if keys[pygame.K_a]:
+                self.rotate(-dt)
+            if keys[pygame.K_d]:
+                self.rotate(dt)
